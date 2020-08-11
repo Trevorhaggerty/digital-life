9fd86db4f18c6399f5c874179e11351ebae70235
Trevorhaggerty <cesismalon@DESKTOP-2CRR68R.localdomain>
2020-08-11 02:11:03 -0700

    more commenting

--- main/ffnnetwork.py
+++ main/ffnnetwork.py
@@ -1,40 +1,42 @@
 from nnode import *
 
 #----------------------------------------------------------------------------------------
-#
+#ffnnetwork is a feed forward neural network. This network is composed of 3 layers of
+#   groups of nnode instances. Divided into an inputlayer, a hiddenlayer, and an outputlayer.
+#   it takes in:
+#       -the size of the input vector
+#       -the size of the input layer
+#       -the size of the hidden layer
+#       -the size of the output layer
+#       -the learning rate
+#   it contains:
+#       -
 class ffnnetwork:
     def __init__(self, numberOfInputs=2, inputLayerCount=2, hiddenLayerCount=2, outputLayerCount=1, learningRate=1):
-        #
+        #the learning rate determines the strength of the backpropagation signal
         self.learningRate = learningRate
-        #
+        #set the number of inputs to the value taken in
         self.numberOfInputs = numberOfInputs
         # create empty arrays for signals and the layers
         self.outputSignals = []
         self.inputLayer = []
         self.hiddenLayer = []
         self.outputLayer = []
-        #
+        #create layers of nodes that have inputs equal to the previous layers outputs
+        #create the input layer with a size determined by inputLayerCount
         for i in range(inputLayerCount):
-            #
-            self.inputLayer.append(
-                #
-                nnode(0, self.learningRate, self.numberOfInputs))
-        #
+            self.inputLayer.append(nnode(0, self.learningRate, self.numberOfInputs))
+        #create the hidden layer with a size determined by inputLayerCount
         for i in range(hiddenLayerCount):
-            #
-            self.hiddenLayer.append(
-                #
-                nnode(0, self.learningRate, len(self.inputLayer)))
-        #
+            self.hiddenLayer.append(nnode(0, self.learningRate, len(self.inputLayer)))
+        #create the output layer with a size determined by inputLayerCount
         for i in range(outputLayerCount):
-            #
-            self.outputLayer.append(
-                #
-                nnode(0, self.learningRate, len(self.hiddenLayer)))
+            self.outputLayer.append(nnode(0, self.learningRate, len(self.hiddenLayer)))
 #----------------------------------------------------------------------------------------
 
     #----------------------------------------------------------------------------------------
-    #
+    #feedforward take the input data and uses the weights, bias, and activation functions of
+    #each node in each layer (input -> input layer -> hiddenlayer -> output layer)
     #
     def feedForward(self, inputData):
         #
--- main/kmean.py
+++ main/kmean.py
@@ -5,134 +5,259 @@ from mathTools import *
 from eventLog import *
 logger = eventLog('kmean', '0.1', 0, False)
 
+
 class kmean() :
+#
     def __init__(self):
+#
         np.random.seed(1)
+#
         self.dataset = []
+#
         self.centroids = np.array([])
-        
+#
 
+#
+
+#
     def loadDataSet(self, dataset, chosenColumns, weights):
+#
         self.chosenColumns = chosenColumns
+#
         self.weights = weights
+#
         self.dataset = []
+#
         for i in range(len(dataset)):
+#
             self.dataset.append([])
+#
 
+#
         for i in range(len(dataset)):
+#
             for j in range(len(dataset[0])):
+#
                 if chosenColumns[j] == 1:
+#
                     self.dataset[i].append(dataset[i][j])
+#
         #print(dataset)       
+#
 
+#
     def placeCentroid(self, k):
+#
         self.k = k
+#
         datasetDomainMin = []
+#
         datasetDomainMax = []
+#
         for i in range(len(self.dataset[0])):
+#
             dbuffer = []
+#
             for j in range(len(self.dataset)):
+#
                 dbuffer.append(self.dataset[j][i])
+#
             datasetDomainMin.append(dbuffer[(np.argmin(np.array(dbuffer)))])
+#
             datasetDomainMax.append(dbuffer[(np.argmax(np.array(dbuffer)))])
+#
         #-------------------------
+#
         #print(str(datasetDomainMin))
+#
         #print(str(datasetDomainMax))
+#
         self.centroids = np.random.rand(self.k, len(self.dataset[0]))
+#
         for i in range(self.k):
+#
             for j in range(len(self.dataset[0])):
+#
                 self.centroids[i][j] = np.random.uniform(datasetDomainMin[j],datasetDomainMax[j])
+#
 
-           
+#
+
+#
     def meanItteration(self, itterationCount = 1):
+#
         for i in range(itterationCount):
+#
             self.kGroups = []
+#
             for i in range(self.k):
+#
                 self.kGroups.append([])       
+#
             for i in range(len(self.dataset)):
+#
                 dbuffer = []
+#
                 for j in range(len(self.centroids)):
+#
                     #print(str(self.dataset[i]))
+#
                     dbuffer.append(wvDistance(self.centroids[j], self.dataset[i], self.weights))
+#
                     #print('distance of vector to perspective centroids = ' + str(dbuffer))
+#
                     #print(str(dataset[i]) + ' is closest to centroid ' + str(np.argmin(np.array(dbuffer))))
+#
                 self.kGroups[(np.argmin(np.array(dbuffer)))].append(self.dataset[i])
+#
 
+#
             for i in range(self.k):
+#
                 translationVector = np.zeros(len(self.dataset[0]))
+#
                 if (len(self.kGroups[i])) >= 1:
+#
                     for j in range(len(self.kGroups[i])):
+#
                         for l in range(len(self.kGroups[i][0])):
+#
                             translationVector[l] += self.kGroups[i][j][l]
+#
                     translationVector = translationVector / (len(self.kGroups[i]))
+#
                     self.centroids[i] = (translationVector + self.centroids[i]) / 2
+#
                 else:
+#
                     dbuffer = []
+#
                     for j in range(len(self.kGroups)):
+#
                         dbuffer.append(len(self.kGroups[j]))
+#
                     #print(str(dbuffer))
+#
                     #print(str(np.argmax(np.array(dbuffer))))
+#
                     translationVector = self.centroids[(np.argmax(np.array(dbuffer)))]
+#
                     self.centroids[i] = (translationVector + self.centroids[i]) / 2
+#
             dbuffer = []
+#
             for j in range(len(self.kGroups)):
+#
                 dbuffer.append(len(self.kGroups[j]))
+#
             #print(str(dbuffer))
+#
 
+#
     def printGroups(self):
+#
         for i in range(self.k):
+#
         #print(str(billy.kGroups[i]))
+#
             for j in range(len(self.kGroups[i])):
+#
                 #print(str(self.ogData[int(self.kGroups[i][j][0])][1]))# + '  ' + str(billy.kGroups[i][j]) )
+#
                 print (self.kGroups[i][j])
+#
             print()
+#
 
+#
 
+#
 class kmeanCSV(kmean):
+#
     def __init__(self):
+#
         super()
+#
         self.ogData = []
+#
 
+#
 
+#
 
+#
     def loadcsvfile(self, csvfile, rowDepth, chosenColumns, weights):
+#
         self.usedColumnNames = []
+#
         self.csvfile = csvfile
+#
         self.dataset = []
+#
         self.weights = weights
+#
         with open(csvfile, newline='') as csvData:
+#
             reader = csv.reader(csvData)
+#
             count = 0
+#
             for row in reader:
+#
                 self.ogData.append(row)
+#
                 databuffer = []
+#
                 if count != 0 and count < rowDepth:
+#
                     for i in range(len(row)):
+#
                         if chosenColumns[i] == 1:
+#
                             stringBuffer = float(row[i])
+#
                             databuffer.append(stringBuffer)
+#
                     self.dataset.append(databuffer)
+#
                 elif count >= rowDepth:
+#
                     break
+#
                 elif count == 0:
+#
                     for i in range(len(row)):
+#
                         if chosenColumns[i] == 1:
+#
                             stringBuffer = (row[i])
+#
                             databuffer.append(stringBuffer)
+#
                     self.usedColumnNames.append(databuffer)
+#
                 count += 1
+#
         #print(str(self.dataset))
+#
 
-    
+#
+
+#
     def printGroups(self):
+#
         for i in range(self.k):
+#
         #print(str(billy.kGroups[i]))
+#
             for j in range(len(self.kGroups[i])):
+#
                 print(str(self.ogData[int(self.kGroups[i][j][0])][1]) + '  ' + str(self.kGroups[i][j]) )
+#
             print()
+#
 
-
-
+#use example-------------------------------------------------------------------------
 
 #dataset = [ [1, 1],
 #            [2, 0],
